8a7226e09bd3760a364e2f6a222af203c86dddd0
NolePowell <nolepowell@hotmail.com>
2024-03-09 15:47:54 -0800

    Updated unifiedAddressFormat in DefaultSearchModel.js

--- server/models/DefaultSearchModel.js
+++ server/models/DefaultSearchModel.js
@@ -4,19 +4,19 @@ const unifiedAddressFormat = {
     country: '', // String: Country name
     name: {
         first_name: '', // String: First name
-        middle_name: '', // String: Middle name
         last_name: '', // String: Last name
-        suffix: '' // String: Suffix (e.g., Jr., Sr.)
     },
     street_address: {
         house_number: '', // String: House number
-        street_name: '', // String: Street name
-        apt_number: '' // String: Apartment number
+        street_name: '', // String: Street name including apartment number if applicable
+        apt_number: '', // String: apartment number if available (sometimes has letters)
     },
-    neighborhood: '', // String: Neighborhood or subarea
-    city: '', // String: City or town or village
-    state_province: '', // String: State or province or ward
-    postal_code: '' // String: Postal code
+    zone_info: {
+        neighborhood: '', // String: Neighborhood or subarea
+        city: '', // String: City or town or village
+        state_province: '', // String: State or province or ward
+        postal_code: '' // String: Postal code
+    }
 };
 
-module.exports = mongoose.model('defaultSearch', unifiedAddressFormat);
+    module.exports = mongoose.model('defaultSearch', unifiedAddressFormat);
